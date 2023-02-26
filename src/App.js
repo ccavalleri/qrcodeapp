bac78305723b6058ca0c84c9a6c30bdaab117ea4
bacetech <ciro.cavalleri@gmail.com>
2023-02-26 17:44:44 +0100

    qrcode app generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
+  const [urlQr, setUrlQr] = useState('https://www.tenutafontana.com/alberata');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: '5px' }}>
+      <h2>QR Code:</h2>
+      <select onChange={(e) => setUrlQr(e.target.value)}>
+        <option value="https://www.tenutafontana.com/alberata">Alberata</option>
+        <option value="https://www.tenutafontana.com/civico-1/">
+          Civico 1
+        </option>
+        <option value="https://www.tenutafontana.com/civico-2/">
+          Civico 2
+        </option>
+        <option value="https://www.tenutafontana.com/civico-28/">
+          Civico 28
+        </option>
+        <option value="https://www.tenutafontana.com/civico-44/">
+          Civico 44
+        </option>
+        <option value="https://www.tenutafontana.com/olmo/">Olmo</option>
+      </select>
+      <div style={{ margin: '15px' }}>
+        <QRCode value={urlQr} />
+      </div>
+      <h2>Indirizzo Web:</h2>
+      <input
+        type="text"
+        value={urlQr}
+        onChange={(e) => setUrlQr(e.target.value)}
+        style={{ width: '400px' }}
+      />
+
+      <div className="copyright">
+        {' '}
+        <hr />© 2021 Bacetech ltd. All Rights Reserved.{' '}
+      </div>
     </div>
   );
 }
